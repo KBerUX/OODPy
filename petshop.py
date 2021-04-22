9f30d045525da7ed323bbddea540aa0db09029b7
KBermoy <kristia.bermoy@gmail.com>
2021-04-21 19:10:01 -0500

    testing

---
+++ petshop.py
@@ -0,0 +1,15 @@
+# Petshop
+
+class petshop:
+    def __init__(self, name, age, breed, color, size):
+        self.name = name
+        self.age = age
+        self.breed = breed
+        self.color = color
+        self.size = size
+
+    def show(self):
+        print(f"My name is: {self.name} and I am {self.age} years old. ")
+
+    def color_breed(self):
+        print(f"My color is: {self.color} and I am a {self.size} animal")
