ffc41bdd85503cfde835ccbe3929bb82c73bbf6d
KBermoy <kristia.bermoy@gmail.com>
2021-04-21 19:24:01 -0500

    added new line

--- petshop.py
+++ petshop.py
@@ -13,3 +13,11 @@ class petshop:
 
     def color_breed(self):
         print(f"My color is: {self.color} and I am a {self.size} animal")
+
+
+class Dog(petshop):
+    def speak("bark")
+
+
+class Cat(petshop):
+    def speak("meow")
